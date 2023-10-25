aee91d6ec140fc24d2dec8b1e1254c642302dae4
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-10-25 14:22:10 +0000

    Deploying to gh-pages from @ PetroMazuryk/financial-Ledger@f29529b1d52dc52ab2536309712fb8601645eb4a 🚀

---
+++ index.3be6103d.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".blog__panel"),l=document.querySelector(".btn__blog"),o=e.querySelector(".blog__close");l.addEventListener("click",(function(){e.classList.add("blog__panel--show")})),o.addEventListener("click",(function(){e.classList.remove("blog__panel--show")}));
+//# sourceMappingURL=index.3be6103d.js.map
---
+++ index.d23f521e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".blog__panel"),o=document.querySelector(".btn__blog"),l=e.querySelector(".blog__close");o.addEventListener("click",(function(){e.classList.add("blog__panel--show")})),l.addEventListener("click",(function(){e.classList.remove("blog__panel--show")}))}();
+//# sourceMappingURL=index.d23f521e.js.map
