945022b315ea64903b94cbcf5eee9fe8254799b9
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-10-26 19:59:06 +0000

    Deploying to gh-pages from @ PetroMazuryk/financial-Ledger@d7dae06fbfff05c03ceec48d57e2187fe0fe83e7 🚀

---
+++ index.01ecdcc5.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.01ecdcc5.js.map
---
+++ index.32955989.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.32955989.js.map
