3b591beb9a658595f704e10adbefa83f3fab5a1e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-12-27 00:45:04 +0200

    fixed code in the lazy-img.js

--- src/js/lazy-img.js
+++ src/js/lazy-img.js
@@ -1,9 +1,27 @@
-const lazyImage = document.querySelectorAll("img[loading='lazy']");
+// const lazyImage = document.querySelectorAll("img[loading='lazy']");
 
-lazyImage.forEach(images => {
-  images.addEventListener('load', onImageLoadet, { once: true });
-});
+// lazyImage.forEach(images => {
+//   images.addEventListener('load', onImageLoaded);
+// });
 
-function onImageLoadet(event) {
-  event.target.classList.add('appear');
-}
+// function onImageLoaded(event) {
+//   event.target.classList.add('appear');
+// }
+
+const boxes = document.querySelectorAll("img[loading='lazy']");
+
+const checkBoxes = () => {
+  const trigger = (window.innerHeight / 5) * 4;
+  for (const box of boxes) {
+    const topOfBox = box.getBoundingClientRect().top;
+    if (topOfBox < trigger) {
+      box.classList.add('appear');
+    } else {
+      box.classList.remove('appear');
+    }
+  }
+};
+
+checkBoxes();
+
+window.addEventListener('scroll', checkBoxes);
